eb65d580774042f92df08d44609478687e834967
Gabrielle <gabrielle97oliveira@gmail.com>
2024-08-27 20:08:50 +0100

    Project simple Stock

---
+++ Projects/Stock/Stock/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Stock
+{
+	public class Product
+	{
+
+		public string Nome;
+		public double Preco;
+		public int Qte;
+
+		public double ValorTotalEmEstoque()
+		{
+			return Preco * Qte;
+		}
+
+		public void AdicionarProdutos(int quantidade)
+		{
+			Qte += quantidade; 
+		}
+
+		public void RemoverProdutos(int quantidade)
+		{
+			Qte -= quantidade;
+		}
+
+        public override string ToString()
+        {
+			return Nome
+				+ ", $"
+				+ Preco.ToString("F2", CultureInfo.InvariantCulture)
+				+ ", "
+				+ Qte
+				+ " unidades, Total: $"
+				+ ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
---
+++ Projects/Stock/Stock/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization; 
+
+namespace Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(); 
+
+            Console.WriteLine("Entre os dados do produto: ");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Qte = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite a quantidade de produtos a ser adicionada ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos a ser removida do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+        }
+    }
+}
