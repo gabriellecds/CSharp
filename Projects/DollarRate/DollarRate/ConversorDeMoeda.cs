11299fc9886361f636386829bbb2784021c5f31e
Gabrielle <gabrielle97oliveira@gmail.com>
2024-09-03 17:02:39 +0100

    Add DollarRate Project

---
+++ Projects/DollarRate/DollarRate/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DollarRate
+{
+	public class ConversorDeMoeda
+	{
+		public double Cotacao;
+		public double Compra;
+
+		public double ValorAPagar()
+		{
+			double Conversao = Cotacao * Compra;
+			return Conversao + (Conversao * 0.06); 
+		}
+
+	}
+}
+
---
+++ Projects/DollarRate/DollarRate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization; 
+
+namespace DollarRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoeda Total = new ConversorDeMoeda();
+
+            Console.Write("Qual é a cotaçao do dólar? ");
+            Total.Cotacao = double.Parse(Console.ReadLine()); 
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            Total.Compra = double.Parse(Console.ReadLine());
+
+
+            Console.Write("Valor a ser pago em reais = R$ " + Total.ValorAPagar().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+        }
+    }
+}
