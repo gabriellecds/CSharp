d96d4b5d61a86a8d4f4d993c48751bd980f9f1b0
Gabrielle <gabrielle97oliveira@gmail.com>
2024-10-10 16:38:00 +0100

    Try-catch examples added

---
+++ Projects/Bank/Bank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using Bank.Entities;
+using Bank.Entities.Exceptions;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Withdraw(withdraw);
+
+                Console.Write("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error: " + e.Message); 
+            }
+        }
+    }
+}
---
+++ Projects/Hotel/Hotel/Program.cs
@@ -0,0 +1,51 @@
+﻿using Hotel.Entities;
+using Hotel.Entities.Exceptions; 
+using System.Globalization; 
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/mm/yyyy): ");
+                DateTime checkin = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Console.Write("Check-out date (dd/mm/yyyy): ");
+                DateTime checkout = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Reservation reservation = new Reservation(number, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter date to update the reservation: ");
+                Console.Write("Check-in date (dd/mm/yyyy): ");
+                checkin = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Console.Write("Check-out date (dd/mm/yyyy): ");
+                checkout = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
