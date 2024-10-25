89523b7d83d17660f751a5da85892f782e265ef1
Gabrielle <gabrielle97oliveira@gmail.com>
2024-10-25 17:13:36 +0100

    Introduction to GetHashCode and Equals

---
+++ Projects/GetHashCode&Equals/GetHashCode&Equals/Program.cs
@@ -0,0 +1,9 @@
+﻿using GetHashCode_Equals.Entities;
+
+Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a == b);
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
