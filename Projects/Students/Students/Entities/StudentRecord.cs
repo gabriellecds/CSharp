705d02ec323340e42e261423ed39f0f61520c33f
Gabrielle <gabrielle97oliveira@gmail.com>
2024-11-01 12:47:54 +0000

    Project Student using Sets added

---
+++ Projects/Students/Students/Entities/StudentRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Students.Entities
+{
+	class StudentRecord
+	{
+		public int Code { get; set; }
+       
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is StudentRecord))
+            {
+                return false; 
+            }
+
+            StudentRecord other = obj as StudentRecord;
+            return Code.Equals(other.Code); 
+        }
+    }
+}
+
---
+++ Projects/Students/Students/Program.cs
@@ -0,0 +1,35 @@
+﻿using Students.Entities;
+using System.IO;
+
+HashSet<StudentRecord> set = new HashSet<StudentRecord>(); 
+
+Console.Write("How many students for course A? ");
+int a = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < a; i++)
+{
+    int code = int.Parse(Console.ReadLine());
+    set.Add(new StudentRecord { Code = code });
+}
+
+Console.Write("How many students for course B? ");
+int b = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < b; i++)
+{
+    int code = int.Parse(Console.ReadLine());
+    set.Add(new StudentRecord { Code = code });
+}
+
+Console.Write("How many students for course C? ");
+int c = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < c; i++)
+{
+    int code = int.Parse(Console.ReadLine());
+    set.Add(new StudentRecord { Code = code });
+}
+
+Console.WriteLine("Total Students: " + set.Count);
+
+
