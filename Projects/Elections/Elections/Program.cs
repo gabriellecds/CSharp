8c4c5464026cb8561ab60673d967dedce4373b87
Gabrielle <gabrielle97oliveira@gmail.com>
2024-11-01 13:40:46 +0000

    Elections project using dictionary added

---
+++ Projects/Elections/Elections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+Console.WriteLine("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{  
+    using (StreamReader sr = File.OpenText(path))
+    {
+        Dictionary<string, int> election = new Dictionary<string, int>();
+
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string key = line[0];
+            int value = int.Parse(line[1]);
+
+            if (election.ContainsKey(key))
+            {
+                election[key] += value; 
+            }
+            else
+            {
+                election[key] = value;
+            }
+
+        }
+
+        foreach(var item in election)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
