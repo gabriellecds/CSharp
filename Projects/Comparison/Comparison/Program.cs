ae88cecf09a2f004eaa51e5e7cc323d401e139c3
Gabrielle <gabrielle97oliveira@gmail.com>
2024-11-02 11:02:46 +0000

    Beginning of studies on dleegates and lambdas

---
+++ Projects/Comparison/Comparison/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Comparison.Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("TV", 900.00));
+list.Add(new Product("Notebook", 1200.00));
+list.Add(new Product("Tablet", 450.00));
+
+// expressao lambda - funcao anonima 
+// Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+// list.Sort(comp);
+list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+// Delegate
+// list.Sort(CompareProduct)
+
+foreach (Product p in list)
+{
+    Console.WriteLine(p);
+}
+
+//Delegate
+// static int CompareProduct(Product p1, Product p2)
+// {
+//      return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper();
+// }
