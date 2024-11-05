1a0bc9e276bee9bbaa747421143106e622c0d375
Gabrielle <gabrielle97oliveira@gmail.com>
2024-11-05 10:39:16 +0000

    Exemple of Predicate added

---
+++ Projects/Predicate/Predicate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Predicate.Entities; 
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+            // com a funcao lambda:
+            // list.RemoveAll(p => p.Price >= 100.00);
+
+            // funcao auxiliar (delegate - predicate):
+            list.RemoveAll(ProductTest); 
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.00;
+        }
+    }
+}
+
